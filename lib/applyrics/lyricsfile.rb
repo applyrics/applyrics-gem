8d06416b3b33bbb1f17d12a6a121ba16710f39e8
Frederik Wallner <frederik.wallner@gmail.com>
2016-04-01 15:37:32 +0200

    Creation of basic lyricsfile

--- lib/applyrics/lyricsfile.rb
+++ lib/applyrics/lyricsfile.rb
@@ -4,8 +4,30 @@ module Applyrics
       def exist?
         File.exist?("./Lyricsfile")
       end
-      def generate
+      def generate(config=nil)
         template = File.read("#{Gem::Specification.find_by_name('applyrics').gem_dir}/lib/assets/LyricsfileTemplate")
+
+        if config.key?(:account_id)
+          template.gsub!('[[ACCOUNT_ID]]', config[:account_id])
+        else
+          template.gsub!('[[ACCOUNT_ID]]', 'your-account-id')
+          template.gsub!('account:', '# account:')
+        end
+
+        if config.key?(:project_key)
+          template.gsub!('[[PROJECT_KEY]]', config[:project_key])
+        else
+          template.gsub!('[[PROJECT_KEY]]', 'your-project-key')
+          template.gsub!('project:', '# project:')
+        end
+
+        if config.key?(:filename)
+          template.gsub!('[[FILENAME]]', config[:filename])
+        else
+          template.gsub!('[[FILENAME]]', 'lyrics.json')
+          template.gsub!('filename:', '# filename:')
+        end
+
         File.write("./Lyricsfile", template)
       end
     end
