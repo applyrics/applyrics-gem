2b20e1422ad36eea928b2ff9d06b4e0c8a29ed6d
Frederik Wallner <frederik.wallner@gmail.com>
2016-04-03 21:55:03 +0200

    Moved ui from setup to cli

--- lib/applyrics/cli.rb
+++ lib/applyrics/cli.rb
@@ -19,9 +19,23 @@ module Applyrics
         c.syntax = "applyrics init"
         c.description = "Sets up the project"
         c.action do |args, options|
-          Applyrics::Setup.new.run()
+
+          config = {}
+          if agree("Connect to Applyrics.io?")
+            ask("Username:")
+            ask("Password:  ") { |q| q.echo = "*" }
+            choice = choose("Project?", :first, :second, :third)
+            config[:account_id] = "awesome-account"
+            config[:project_key] = choice.to_s
+          elsif agree("Use local files?")
+            config[:filename] = "lyrics.json"
+          end
+
+          project = Applyrics::Setup.new.run(config)
+
           if agree("Rebuild language files?")
             puts "will rebuild..."
+            project.rebuild_files()
           end
         end
       end
@@ -34,6 +48,27 @@ module Applyrics
           options.default :project => './'
           puts "rebuilding..."
           Applyrics::GenStrings.run("#{options.project}", "#{options.project}")
+        end
+      end
+
+      command :apply do |c|
+        c.syntax = "applyrics apply"
+        c.description = "Applies language from a .json file"
+        c.option '--project STRING', String, 'Path to iOS or Android project'
+        c.option '--data STRING', String, 'Path to .json file (Default: lyrics.json)'
+        c.action do |args, options|
+          options.default :project => './', :data => './lyrics.json'
+          puts "Not implemented yet... Sorry!"
+        end
+      end
+
+      command :sync do |c|
+        c.syntax = "applyrics sync"
+        c.description = "Syncs language from applyrics.io"
+        c.option '--project STRING', String, 'Path to iOS or Android project'
+        c.action do |args, options|
+          options.default :project => './'
+          puts "Not implemented yet... Sorry!"
         end
       end
 
--- lib/applyrics/setup.rb
+++ lib/applyrics/setup.rb
@@ -4,7 +4,7 @@ require 'applyrics/lyricsfile'
 module Applyrics
   class Setup
 
-    def run()
+    def run(config = {})
       platform = nil
       if is_ios?
         puts "Found iOS project..."
@@ -18,17 +18,6 @@ module Applyrics
       else
         puts "Didn't find any project!"
         return
-      end
-
-      config = {}
-      if agree("Connect to Applyrics.io?")
-        ask("Username:")
-        ask("Password:  ") { |q| q.echo = "*" }
-        choice = choose("Project?", :first, :second, :third)
-        config[:account_id] = "awesome-account"
-        config[:project_key] = choice.to_s
-      elsif agree("Use local files?")
-        config[:filename] = "lyrics.json"
       end
 
       Applyrics::Lyricsfile.generate(config)
