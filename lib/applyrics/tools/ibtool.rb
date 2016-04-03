90d33fbab4dac9ce4126f8fabd98a84c60bd5f6c
Frederik Wallner <frederik.wallner@gmail.com>
2016-04-03 22:04:46 +0200

    First version of ibtool

--- lib/applyrics/tools/ibtool.rb
+++ lib/applyrics/tools/ibtool.rb
@@ -1,4 +1,15 @@
+require 'applyrics/command'
 module Applyrics
   class IBTool
+    class << self
+      def run(folder, output_folder=nil)
+        output_folder = folder if output_folder.nil?
+
+        folder = File.expand_path(folder)
+        output_folder = File.expand_path(output_folder)
+
+        
+      end
+    end
   end
 end
