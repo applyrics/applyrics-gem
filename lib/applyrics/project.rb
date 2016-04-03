75c1f8a550bba5c0fb7843521af54665f18bf009
Frederik Wallner <frederik.wallner@gmail.com>
2016-04-03 21:25:09 +0200

    Updated doc for project

--- lib/applyrics/project.rb
+++ lib/applyrics/project.rb
@@ -13,13 +13,20 @@ module Applyrics
       @path = path
     end
 
-    # Return a list of detected languages in the project
+    # @return [Array] An array of languages detected in the project.
     def detected_languages
       @project.detected_languages()
     end
 
+    # @param [String] the name of the setting to read.
+    # @return [String, nil] the value of the setting or nil if not found.
     def platform_project_settings(name)
       @project.platform_project_settings(name)
+    end
+
+    # Rebuild the language files.
+    def rebuild_files
+      @project.rebuild_files()
     end
   end
 end
